bb915fb611e3fbf8fdb49e729945555ffa184c4e
Orlando Vazquez <ovazquez@gmail.com>
2012-03-22 17:32:50 -0700

    CNAPI-7: Hook up CNAPI to Ur via AMQP

--- bin/cnapi.js
+++ bin/cnapi.js
@@ -4,44 +4,7 @@
  * Main entry-point for the CNAPI.
  */
 
-var createUfdsModel = require('../lib/models').createUfdsModel;
-var createServer = require('../lib/server').createServer;
-var async = require('async');
+var CNAPI = require('../lib/cnapi');
 
-function main() {
-    var server;
-    var model;
-
-    var modelOptions = {
-        ufdsSettings: {
-            host: 'ufds_host',
-            port: 12345,
-            user: 'ufds_user',
-            user: 'ufds_pass'
-        }
-    };
-
-    async.waterfall([
-        function (wf$callback) {
-            model = createUfdsModel(modelOptions);
-            // model.onError(...);
-            model.connect(function () {
-                return wf$callback();
-            });
-        },
-        function (wf$callback) {
-            var serverOptions = {
-                model: model
-            };
-            server = createServer(serverOptions);
-            return wf$callback();
-        }
-    ],
-    function (error) {
-        server.listen(8080, function () {
-          console.log('%s listening at %s', server.name, server.url);
-        });
-    });
-}
-
-main();
+var cnapi = new CNAPI();
+cnapi.start();
