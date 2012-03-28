c63e4f9b4348f32f850db42783d343309d401e44
Orlando Vazquez <ovazquez@gmail.com>
2012-03-27 17:17:35 -0700

    Add more tests

--- test/test-model.js
+++ test/test-model.js
@@ -7,34 +7,63 @@ var createUfdsModel = require('../lib/models').createUfdsModel;
 var configFilename = path.join(__dirname, '..', 'config', 'config.coal.json');
 
 module.exports = {
-    "list servers": function (test) {
+    setUp: function (callback) {
+        var self = this;
         var config;
-        var model;
-
+        
         async.waterfall([
             function (wf$callback) {
                 common.loadConfig(configFilename, function (error, c) {
                     config = c;
+                    return wf$callback();
                 });
             },
             function (wf$callback) {
                 console.dir(config);
-                model = createUfdsModel({ ufds: config.ufds });
-                model.connect(function () {
+                self.model = createUfdsModel({ ufds: config.ufds });
+                self.model.connect(function () {
                     console.info("Connected to ufds model");
-                    wf$callback();
+                    return wf$callback();
                 });
             },
-            function (wf$callback) {
-                model.listServers({}, function (error, servers) {
-                    console.info("Listed servers");
-                    console.dir(servers);
-                    test.equal(servers.length, 1, "Servers were returned in query");
-                    wf$callback();
-                });
-            }
         ],
         function (error) {
+            return callback(error);
+        });
+    },
+    "tearDown": function (callback) {
+        var self = this;
+        self.model.disconnect(function () {
+           callback();
+        });
+    },
+    "list servers in all datacenters": function (test) {
+        var self = this;
+        self.model.listServers({}, function (error, servers) {
+            console.info("Listed servers");
+            console.dir(servers);
+            test.equal(servers.length, 1, "Should see servers returned");
+            test.equal(error, undefined, "Should not get any errors");
+            test.done();
+        });
+    },
+    "list servers in coal": function (test) {
+        var self = this;
+        self.model.listServers({ datacenter: "coal" }, function (error, servers) {
+            console.info("Listed servers");
+            console.dir(servers);
+            test.equal(servers.length, 1, "Should see servers returned");
+            test.equal(error, undefined, "Should not get any errors");
+            test.done();
+        });
+    },
+    "list servers in non-existent datacenter": function (test) {
+        var self = this;
+        self.model.listServers({ datacenter: "idontexist" }, function (error, servers) {
+            console.info("Listed servers");
+            console.dir(servers);
+            test.equal(servers.length, 0, "Should see no servers returned");
+            test.equal(error, undefined, "Should not get any errors");
             test.done();
         });
     }
