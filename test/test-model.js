37b4c0485ecb67ab37676b7cd2e5fb42b2cc706d
Orlando Vazquez <ovazquez@gmail.com>
2012-04-02 01:21:37 -0700

    Squashed commit of the following (CNAPI-10, CNAPI-8, CNAPI-7):
    commit 78377645945c238239ab54ca8b512c7351da4250
    Author: Orlando Vazquez <ovazquez@gmail.com>
    Date:   Mon Apr 2 01:16:48 2012 -0700
    
        CNAPI-10: Add bunyan-based logging to CNAPI
    
    commit a997752d629500702c08d4571fbeafeb0bc73bf1
    Author: Orlando Vazquez <ovazquez@gmail.com>
    Date:   Thu Mar 29 00:17:42 2012 -0700
    
        Get CNAPI updating UFDS with server sysinfo obtained through Ur
    
    commit c63e4f9b4348f32f850db42783d343309d401e44
    Author: Orlando Vazquez <ovazquez@gmail.com>
    Date:   Tue Mar 27 17:17:35 2012 -0700
    
        Add more tests
    
    commit 5145ae2d2de016468acf78283fc086dd9a6e3248
    Author: Orlando Vazquez <ovazquez@gmail.com>
    Date:   Tue Mar 27 16:34:22 2012 -0700
    
        get dependencies added

---
+++ test/test-model.js
@@ -0,0 +1,61 @@
+var path = require('path');
+var async = require('async');
+
+var common = require('../lib/common');
+var createUfdsModel = require('../lib/models').createUfdsModel;
+
+var configFilename = path.join(__dirname, '..', 'config', 'config.coal.json');
+
+module.exports = {
+    setUp: function (callback) {
+        var self = this;
+        var config;
+
+        async.waterfall([
+            function (wf$callback) {
+                common.loadConfig(configFilename, function (error, c) {
+                    config = c;
+                    return wf$callback();
+                });
+            },
+            function (wf$callback) {
+                self.model = createUfdsModel({ ufds: config.ufds });
+                self.model.connect(function () {
+                    console.info('Connected to ufds model');
+                    return wf$callback();
+                });
+            }
+        ],
+        function (error) {
+            return callback(error);
+        });
+    },
+    'tearDown': function (callback) {
+        var self = this;
+        self.model.disconnect(function () {
+           callback();
+        });
+    },
+    'list servers in all datacenters': function (test) {
+        var self = this;
+        self.model.listServers({}, function (error, servers) {
+            console.info('Listed servers');
+            console.dir(servers);
+            test.equal(servers.length, 1, 'Should see servers returned');
+            test.equal(error, undefined, 'Should not get any errors');
+            test.done();
+        });
+    },
+    'list servers in coal': function (test) {
+        var self = this;
+        self.model.listServers(
+            { datacenter: 'coal' },
+            function (error, servers) {
+                console.info('Listed servers');
+                console.dir(servers);
+                test.equal(servers.length, 1, 'Should see servers returned');
+                test.equal(error, undefined, 'Should not get any errors');
+                test.done();
+            });
+    }
+};
