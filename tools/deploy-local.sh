61d98e10bdd29ac8d7fdb27c56dc37650ed9b4c8
Orlando Vazquez <ovazquez@gmail.com>
2012-04-30 03:37:14 -0700

    CNAPI-27: CNAPI should be aware of what datacenter it is running in

---
+++ tools/deploy-local.sh
@@ -0,0 +1,4 @@
+DIRNAME=$(cd `dirname $0`/.. && pwd)
+cd $DIRNAME
+rsync --recursive --partial -l ./{bin,lib,node_modules} /zones/`sdc-login cnapi zonename`/root/opt/smartdc/cnapi
+sdc-login cnapi 'svcadm restart cnapi; svcadm clear cnapi; sleep 5; tail -n 50 `svcs -L cnapi`; svcs cnapi'
