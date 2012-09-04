f09e064aa94933ae1f77ff803be50591b5c0a2f8
Orlando Vazquez <ovazquez@gmail.com>
2012-09-04 02:56:14 -0700

    CNAPI-90: Break up monolithic CNAPI "model" into logical components

--- tools/deploy-local.sh
+++ tools/deploy-local.sh
@@ -5,5 +5,5 @@ cd $DIRNAME
 rsync --recursive --partial -l ./{config,bin,lib,test} /zones/`sdc-login cnapi zonename`/root/opt/smartdc/cnapi
 
 if [[ -z "$NO_RESTART" ]]; then
-    sdc-login cnapi 'svcadm restart cnapi; svcadm clear cnapi; sleep 5; tail -n 50 `svcs -L cnapi`; svcs cnapi'
+    sdc-login cnapi 'svcadm restart cnapi; svcadm clear cnapi; tail -n 50 `svcs -L cnapi`; svcs cnapi'
 fi
