689efdebcc31c3254af0c9d7bcaaeccfa78bee78
Orlando Vazquez <ovazquez@gmail.com>
2012-10-29 15:15:01 -0700

    Get fetching working

--- tools/deploy-local.sh
+++ tools/deploy-local.sh
@@ -5,5 +5,5 @@ cd $DIRNAME
 rsync --recursive --partial -l ./{Makefile,deps,config,bin,lib,test,package.json} /zones/`sdc-login cnapi zonename`/root/opt/smartdc/cnapi
 
 if [[ -z "$NO_RESTART" ]]; then
-    sdc-login cnapi 'svcadm restart cnapi; svcadm clear cnapi; tail -n 50 `svcs -L cnapi`; svcs cnapi'
+    sdc-login cnapi 'svcadm enable cnapi; svcadm restart cnapi; svcadm clear cnapi; tail -n 50 `svcs -L cnapi`; svcs cnapi'
 fi
