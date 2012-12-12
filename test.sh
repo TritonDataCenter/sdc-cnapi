3dd2dfb5ebc0bb4b98579efb6857f963bcbf8fad
Orlando Vazquez <ovazquez@gmail.com>
2012-12-12 18:52:45 +0000

    CNAPI-156: Set some more aggressive timeouts on vm load endpoint

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ else
 fi
 
 RSYNC_CMD="rsync --recursive --partial -l ./{config,bin,lib,test} /zones/`sdc-login cnapi zonename`/root/opt/smartdc/cnapi"
-RUNTEST_CMD="cd /opt/smartdc/cnapi && ./test/runtest -r default"
+RUNTEST_CMD="cd /opt/smartdc/cnapi && ./test/runtests -r default"
 RUNTEST_CMD_ZONE="sdc-login cnapi '$RUNTEST_CMD'"
 
 if [[ "$ZONENAME" == "global" ]]; then
