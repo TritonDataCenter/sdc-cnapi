46a5c6f4fd2598ddea301324e001846d5a6c1fa8
Orlando Vazquez <ovazquez@gmail.com>
2013-06-07 01:26:54 +0000

    CNAPI-276: Merge model.js and cnapi.js => app.js (and their respective objects)

--- bin/cnapi.js
+++ bin/cnapi.js
@@ -4,12 +4,12 @@
  * Main entry-point for the CNAPI.
  */
 
-var CNAPI = require('../lib/cnapi');
+var App = require('../lib/app');
 var common = require('../lib/common');
 var path = require('path');
 
 var configFilename = path.join(__dirname, '..', 'config', 'config.json');
 common.loadConfig(configFilename, function (error, config) {
-    var cnapi = new CNAPI(config);
-    cnapi.start();
+    var app = new App(config);
+    app.start();
 });
