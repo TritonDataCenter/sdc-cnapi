a9993e0fe30aba3389ca9a974d6532646d7b0224
Marsell Kukuljevic <marsell@joyent.com>
2014-03-26 02:51:09 +1100

    CNAPI-365: improve tests for /servers endpoint.

--- test/test-servers.js
+++ test/test-servers.js
@@ -4,6 +4,7 @@
  * test-servers.js: Tests for servers endpoint.
  */
 
+var async   = require('async');
 var http    = require('http');
 var restify = require('restify');
 
@@ -24,19 +25,13 @@ function setup(callback) {
 }
 
 
-// this test assumes all CNs known by CNAPI are setup
-function testListServersWithCapacity(t) {
-    client.get('/servers?extras=capacity', function (err, req, res, body) {
+function testListServers(t) {
+    client.get('/servers', function (err, req, res, body) {
         t.ifError(err);
         t.ok(body);
 
         body.forEach(function (server) {
-            t.ok(typeof (server.unreserved_cpu)  === 'number');
-            t.ok(typeof (server.unreserved_ram)  === 'number');
-            t.ok(typeof (server.unreserved_disk) === 'number');
-
-            t.ifError(server.sysinfo);
-            t.ifError(server.vms);
+            validateServer(t, server, {});
         });
 
         t.done();
@@ -44,19 +39,89 @@ function testListServersWithCapacity(t) {
 }
 
 
-// this test assumes all CNs known by CNAPI are setup
-function testListServersWithAll(t) {
+function testListServersWithVms(t) {
+    client.get('/servers?extras=vms', function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+
+        body.forEach(function (server) {
+            validateServer(t, server, { vms: true });
+        });
+
+        t.done();
+    });
+}
+
+
+function testListServersWithDisk(t) {
+    client.get('/servers?extras=disk', function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+
+        body.forEach(function (server) {
+            validateServer(t, server, { disk: true });
+        });
+
+        t.done();
+    });
+}
+
+
+function testListServersWithMemory(t) {
+    client.get('/servers?extras=memory', function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+
+        body.forEach(function (server) {
+            validateServer(t, server, { memory: true });
+        });
+
+        t.done();
+    });
+}
+
+
+function testListServersWithCapacity(t) {
+    client.get('/servers?extras=capacity', function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+
+        body.forEach(function (server) {
+            validateServer(t, server, { capacity: true, disk: true,
+                                        memory: true });
+        });
+
+        t.done();
+    });
+}
+
+
+function testListServersWithAll1(t) {
     client.get('/servers?extras=all', function (err, req, res, body) {
         t.ifError(err);
         t.ok(body);
 
         body.forEach(function (server) {
-            t.ok(typeof (server.unreserved_cpu)  === 'number');
-            t.ok(typeof (server.unreserved_ram)  === 'number');
-            t.ok(typeof (server.unreserved_disk) === 'number');
+            validateServer(t, server, { sysinfo: true, vms: true,
+                                        capacity: true, disk: true,
+                                        memory: true });
+        });
 
-            t.ok(server.sysinfo);
-            t.ok(server.vms);
+        t.done();
+    });
+}
+
+
+function testListServersWithAll2(t) {
+    client.get('/servers?extras=vms,sysinfo,disk,memory,capacity',
+               function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+
+        body.forEach(function (server) {
+            validateServer(t, server, { sysinfo: true, vms: true,
+                                        capacity: true, disk: true,
+                                        memory: true });
         });
 
         t.done();
@@ -74,19 +139,182 @@ function testGetServer(t) {
         client.get('/servers/' + uuid, function (err2, req2, res2, body2) {
             t.ifError(err2);
 
-            t.ok(typeof (body2.unreserved_cpu)  === 'number');
-            t.ok(typeof (body2.unreserved_ram)  === 'number');
-            t.ok(typeof (body2.unreserved_disk) === 'number');
-
+            validateServer(t, body2, { sysinfo: true, vms: true,
+                                       capacity: true, disk: true,
+                                       memory: true });
             t.done();
         });
     });
 }
 
 
+function testUpdateServer(t) {
+    var uuid;
+    var oldRatio;
+
+    async.waterfall([
+        function (next) {
+            client.get('/servers?headnode=true',
+                       function (err, req, res, body) {
+                if (err) {
+                    next(err);
+                    return;
+                }
+
+                uuid = body[0].uuid;
+                oldRatio = body[0].reservation_ratio;
+
+                next();
+            });
+        },
+        function (next) {
+            var changes = { reservation_ratio: 0.50 };
+
+            client.post('/servers/' + uuid, changes,
+                        function (err, req, res, body) {
+                next(err);
+            });
+        },
+        function (next) {
+            client.get('/servers/' + uuid, function (err, req, res, body) {
+                if (err) {
+                    next(err);
+                    return;
+                }
+
+                t.equal(body.reservation_ratio, 0.50);
+
+                next();
+            });
+        }
+    ], function (err) {
+        var changes = { reservation_ratio: oldRatio };
+
+        client.post('/servers/' + uuid, changes,
+                    function (err2, req, res, body) {
+            t.ifError(err);
+            t.ifError(err2);
+            t.done();
+        });
+    });
+}
+
+
+var UUID_RE = /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/;
+var VALID_SERVER_OVERPROVISION_RESOURCES = ['cpu', 'ram', 'disk', 'io', 'net'];
+
+function validateServer(t, server, options) {
+    t.equal(typeof (server.setup), 'boolean');
+    if (!server.setup) {
+        return;
+    }
+
+    t.ok(UUID_RE.test(server.uuid));
+    t.equal(typeof (server.reserved), 'boolean');
+    t.equal(typeof (server.reservation_ratio), 'number');
+
+    var diskAttr = [
+        'disk_pool_size_bytes', 'disk_installed_images_used_bytes',
+        'disk_zone_quota_bytes', 'disk_kvm_quota_bytes'
+    ];
+
+    if (options.disk) {
+        diskAttr.forEach(function (attr) {
+            t.equal(typeof (server[attr]), 'number');
+        });
+    } else {
+        diskAttr.forEach(function (attr) {
+            t.ifError(server[attr]);
+        });
+    }
+
+    var memoryAttr = ['memory_available_bytes', 'memory_total_bytes'];
+    if (options.memory) {
+        memoryAttr.forEach(function (attr) {
+            t.equal(typeof (server[attr]), 'number');
+        });
+    } else {
+        memoryAttr.forEach(function (attr) {
+            t.ifError(server[attr]);
+        });
+    }
+
+
+    if (server.traits) {
+        var traits = server.traits;
+        t.ok(typeof (traits) === 'object' && !Array.isArray(traits));
+    }
+
+    if (server.overprovision_ratios) {
+        var ratios = server.overprovision_ratios;
+
+        t.ok(typeof (ratios) === 'object' && !Array.isArray(ratios));
+
+        var ratioResources = Object.keys(ratios);
+
+        for (var i = 0; i !== ratioResources.length; i++) {
+            var resource = ratioResources[i];
+            var ratio = ratios[resource];
+
+            t.ok(VALID_SERVER_OVERPROVISION_RESOURCES.indexOf(resource) !== -1);
+            t.equal(typeof (ratio), 'number');
+        }
+    }
+
+    var sysinfo = server.sysinfo;
+    if (options.sysinfo) {
+        t.ok(typeof (sysinfo) === 'object' && !Array.isArray(sysinfo));
+        t.equal(typeof (server.sysinfo['CPU Total Cores']), 'number');
+    } else {
+        t.ifError(sysinfo);
+    }
+
+    var capAttr = ['unreserved_cpu', 'unreserved_ram', 'unreserved_disk'];
+    if (options.capacity) {
+        capAttr.forEach(function (attr) {
+            t.equal(typeof (server[attr]), 'number');
+        });
+    } else {
+        capAttr.forEach(function (attr) {
+            t.ifError(server[attr]);
+        });
+    }
+
+    var vms = server.vms;
+    if (options.vms) {
+        t.ok(typeof (vms) === 'object' && !Array.isArray(vms));
+
+        var vmUuids = Object.keys(vms);
+
+        for (i = 0; i != vmUuids.length; i++) {
+            var vmUuid = vmUuids[i];
+            var vm = vms[vmUuid];
+
+            t.ok(UUID_RE.test(vm.owner_uuid));
+
+            var numAttr = ['max_physical_memory', 'quota', 'cpu_cap'];
+            numAttr.forEach(function (attr) {
+                t.equal(typeof (vm[attr]), 'number');
+            });
+
+            t.equal(typeof (vm.last_modified), 'string');
+            t.equal(typeof (vm.state), 'string');
+        }
+    } else {
+        t.ifError(vms);
+    }
+}
+
+
 module.exports = {
     setUp: setup,
+    'list servers': testListServers,
+    'list servers with vms': testListServersWithVms,
+    'list servers with memory': testListServersWithMemory,
+    'list servers with disk': testListServersWithDisk,
     'list servers with capacity': testListServersWithCapacity,
-    'list servers with all': testListServersWithAll,
-    'get server': testGetServer
+    'list servers with all 1': testListServersWithAll1,
+    'list servers with all 2': testListServersWithAll2,
+    'get server': testGetServer,
+    'update server': testUpdateServer
 };
