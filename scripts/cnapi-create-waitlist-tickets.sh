d803dc56e630e98b2f017da2abaec331790dfb14
Orlando Vazquez <ovazquez@gmail.com>
2014-06-27 10:07:45 +0000

    CNAPI-393: rewrite waitlist subsystem to be HA-safe

--- scripts/cnapi-create-waitlist-tickets.sh
+++ scripts/cnapi-create-waitlist-tickets.sh
@@ -1,5 +1,8 @@
 set -o xtrace
-expiry_far=$(/usr/node/bin/node -e 'console.log(new Date((new Date().valueOf()) + 120*1000).toISOString())')
+expiry_far=$(/usr/node/bin/node -e 'console.log(new Date((new Date().valueOf()) + 10*1000).toISOString())')
 obj="{ \"scope\": \"foo\", \"id\": \"bar\", \"expires_at\": \"$expiry_far\" }"
-sdc-cnapi /servers/$(sysinfo |json UUID)/tickets -X POST -d "$obj"
-sdc-cnapi /servers/$(sysinfo |json UUID)/tickets -X POST -d "$obj"
+NUM=$1
+
+for i in $(seq 1 $NUM); do
+    sdc-cnapi /servers/$(sysinfo |json UUID)/tickets -X POST -d "$obj" &
+done
