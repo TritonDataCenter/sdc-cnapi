6de786e970fcea5560eb01e48908cf4118ad2e13
Orlando Vazquez <ovazquez@gmail.com>
2014-06-27 10:07:45 +0000

    CNAPI-393: rewrite waitlist subsystem to be HA-safe

--- scripts/cnapi-delete-all-servers.sh
+++ scripts/cnapi-delete-all-servers.sh
@@ -7,6 +7,9 @@ load_sdc_config
 
 sdc-login cnapi svcadm disable cnapi
 ./tools/deploy-local.sh
-sdc-login cnapi /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_servers
-sdc-login cnapi /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_waitlist_tickets
+sdc-login cnapi /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_servers &
+sdc-login cnapi /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_waitlist_tickets &
+sdc-login cnapi /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_waitlist_queues &
+
+wait
 sdc-login cnapi svcadm enable cnapi
