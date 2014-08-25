758f1d0441147e39cbf36aae04a7930ba1189ffe
Orlando Vazquez <ovazquez@gmail.com>
2014-08-25 21:03:12 +0000

    TOOLS-607 I for one, welcome my new open source license

--- scripts/cnapi-create-waitlist-tickets.sh
+++ scripts/cnapi-create-waitlist-tickets.sh
@@ -1,3 +1,13 @@
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright (c) 2014, Joyent, Inc.
+#
+
 set -o xtrace
 expiry_far=$(/usr/node/bin/node -e 'console.log(new Date((new Date().valueOf()) + 10*1000).toISOString())')
 obj="{ \"action\": \"reboot\", \"scope\": \"foo\", \"id\": \"bar\", \"expires_at\": \"$expiry_far\" }"
