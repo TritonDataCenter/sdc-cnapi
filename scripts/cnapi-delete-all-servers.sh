71624cb08cf3dd782e0764e5c4806cec89115fa7
Orlando Vazquez <ovazquez@gmail.com>
2015-06-24 02:35:14 -0700

    CNAPI-538: use a separate bucket to store last_heartbeat values

--- scripts/cnapi-delete-all-servers.sh
+++ scripts/cnapi-delete-all-servers.sh
@@ -14,11 +14,13 @@ set -x xtrace
 . /lib/sdc/config.sh
 load_sdc_config
 
-sdc-login cnapi svcadm disable cnapi
-./tools/deploy-local.sh
-sdc-login cnapi /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_servers &
-sdc-login cnapi /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_waitlist_tickets &
-sdc-login cnapi /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_waitlist_queues &
+UUID=$(sdc-vmname cnapi)
+
+zlogin $UUID svcadm disable cnapi
+#./tools/deploy-local.sh
+zlogin $UUID /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_servers &
+zlogin $UUID /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_waitlist_tickets &
+zlogin $UUID /opt/smartdc/cnapi/{build/node/bin/node,node_modules/.bin/delbucket} -h $CONFIG_moray_admin_ips cnapi_waitlist_queues &
 
 wait
-sdc-login cnapi svcadm enable cnapi
+zlogin $UUID svcadm enable cnapi
