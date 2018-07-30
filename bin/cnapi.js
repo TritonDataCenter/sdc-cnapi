eccae735ed94d927225eb119ebbeb6e3f5a344bb
Josh Wilsdon <josh@wilsdon.ca>
2018-07-30 16:43:34 -0700

    fix naming

--- bin/cnapi.js
+++ bin/cnapi.js
@@ -54,8 +54,7 @@ common.loadConfig(configFilename, function (error, config) {
     });
 
     metricsManager.createRestifyMetrics();
-    metricsManager.createGCMetrics();
-    metricsManager.createNodeMetrics();
+    metricsManager.createNodejsMetrics();
     metricsManager.listen(function metricsServerStarted() {
         app = new App(config, {
             log: cnapiLog,
