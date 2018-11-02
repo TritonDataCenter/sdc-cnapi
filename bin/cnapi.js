62a76fbea305b21b2fd31818286af5cdd032493f
Josh Wilsdon <josh@wilsdon.ca>
2018-11-01 22:29:02 -0700

    use non-default metricOpts to show this can work

--- bin/cnapi.js
+++ bin/cnapi.js
@@ -15,13 +15,14 @@
 var path = require('path');
 
 var bunyan = require('bunyan');
-var createMetricsManager = require('triton-metrics').createMetricsManager;
 var restify = require('restify');
+var tritonMetrics = require('triton-metrics');
 
 var App = require('../lib/app');
 var common = require('../lib/common');
 
 var configFilename = path.join(__dirname, '..', 'config', 'config.json');
+var createMetricsManager = tritonMetrics.createMetricsManager;
 var METRICS_SERVER_PORT = 8881;
 
 
@@ -45,6 +46,26 @@ common.loadConfig(configFilename, function (error, config) {
         log: cnapiLog.child({component: 'metrics'}),
         port: METRICS_SERVER_PORT,
         restify: restify,
+        metricOpts: {
+            http_request_duration_seconds: {
+                // The buckets here match the defaults in node-triton-metrics,
+                // but we set them anyway and set a buckets_version so that we
+                // can change them independently if we want to.
+                buckets: tritonMetrics.logLinearBuckets(10, -5, 3, 5)
+                    .filter(function _trimBuckets(v) {
+                        // Limit to >= 0.001 and <= 10000 as this is the range of
+                        // observed values for all Triton APIs we surveyed
+                        // across a month of production data.
+                        if (v > 10000 || v < 0.001) {
+                            return false;
+                        }
+                        return true;
+                    }),
+                labels: {
+                    buckets_version: 'cnapi.1'
+                }
+            }
+        },
         staticLabels: {
             datacenter: config.datacenter_name,
             instance: config.instanceUuid,
